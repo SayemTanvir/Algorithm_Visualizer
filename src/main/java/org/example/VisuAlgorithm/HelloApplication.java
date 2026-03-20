b4c0d173a979b25985e3d0a737461bc65837863c
SayemTanvir <sayemtanvir341@gmail.com>
2026-03-20 17:58:27 +0600

    Exceptions handled

--- src/main/java/org/example/VisuAlgorithm/HelloApplication.java
+++ src/main/java/org/example/VisuAlgorithm/HelloApplication.java
@@ -5,24 +5,37 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(
-                HelloApplication.class.getResource("style.css").toExternalForm()
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("hello-view.fxml")
         );
-        stage.setTitle("Hello!");
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        // CSS
+//        scene.getStylesheets().add(
+//                getClass().getResource("style.css").toExternalForm()
+//        );
+
+        stage.setTitle("VisualAlgorithm");
+
+        // Icon (safe)
+        try {
+            Image image = new Image(getClass().getResourceAsStream("/Images/sorting.png"));
+            stage.getIcons().add(image);
+        } catch (Exception e) {
+            System.out.println("Icon not found");
+        }
+
         stage.setScene(scene);
-
-        Image image = new Image(getClass().getResourceAsStream("/Images/sorting.png"));
-        stage.getIcons().add(image);
-
+        stage.setMaximized(true); // optional
 
         stage.show();
     }
